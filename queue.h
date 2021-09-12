30c89dd82c013a953f18f467a09327f63259b98e
jose <jose.sanchez.a@utec.edu.pe>
2021-09-12 17:17:14 -0500

    muchisimos más avances

--- queue.h
+++ queue.h
@@ -2,20 +2,16 @@
 
 template <typename T>
 class QueueArray : public CircularArray<T> {
-    public:
-        QueueArray() : CircularArray<T>() {}
-
-        QueueArray(int size) : CircularArray<T>(size) {}
+    public:        
+        void enqueue(T value){
+            push_back(value);
+        }
         
-        void enqueue(T value) : push_back(value) {}
-
-        T dequeue() : pop_front() {}
-
-        /*
+        T dequeue(){
+            return pop_front();
+        }
+    
         T front() {
             return array[front];
         }
-        */
-
-        ~QueueArray() : ~CircularArray<T>() {}
 };
--- stack.h
+++ stack.h
@@ -2,20 +2,16 @@
 
 template <typename T>
 class StackArray : public CircularArray<T> {
-    public:
-        StackArray() : CircularArray<T>() {};
-
-        StackArray(int size) : CircularArray<T>(size) {}
-        
-        void push(T value) : push_back(value) {}
-
-        T pop() : pop_back() {}
-
-        /*
-        T top() {
-            return array[back];
+    public:        
+        void push(T value){
+            push_front(value);
         }
-        */
 
-        ~StackArray() : ~CircularArray<T>() {}
+        T pop(){
+            return pop_front();
+        }
+
+        T top() {
+            return array[front];
+        }
 };
